8fb04178bd33292f6823165249f1a9eba75212c4
Abel Mulugeta <abelmulugeta2014@gmail.com>
2018-06-16 15:20:52 +0300

    Finished basic server

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const AdminRouter = require('./lib/routes/AdminRouter');
+const ApiRouter = require('./lib/routes/ApiRouter');
+const UserRouter = require('./lib/routes/UserRouter');
+
+require('./lib/connection');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// --------------------Control which can access our web port----------------------
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+// ----------------------Routes---------------------
+app.use('/api', ApiRouter);
+app.use('/user', UserRouter);
+app.use('/admin', AdminRouter);
+
+app.listen(8080, () => global.console.log('magic happens at 8080'));
